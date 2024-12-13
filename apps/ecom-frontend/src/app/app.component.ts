337eb48227c62bae2c4ac7d7fdcb1736cbb95c7d
octaviookumu <octaviookumu@gmail.com>
2024-12-13 16:48:18 +0300

    Set up Navbar

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  FaConfig,
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FontAwesomeModule, NavbarComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,5 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+
+// only import the icons needed
+export const fontAwesomeIcons: IconDefinition[] = [faUser, faCartShopping];
